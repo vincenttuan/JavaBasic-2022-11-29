34d514b10b387ee92849eb69ef95b8b6e707bc5a
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-01 19:53:54 +0800

    Add files via upload

--- day2/Demo1.java
+++ day2/Demo1.java
@@ -3,14 +3,18 @@ package day2;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		byte a = 10;
+		byte  a = 10;
 		short b = 28000;
-		int c = 100_0000;
-		long d = 200_0000_0000L;
+		int   c = 100_0000;
+		long  d = 200_0000_0000L;
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
 		System.out.println(d);
+		System.out.printf("%,d\n", a); // f -> format
+		System.out.printf("%,d\n", b);
+		System.out.printf("%,d\n", c);
+		System.out.printf("%,d\n", d);
 	}
 
 }
---
+++ day2/Demo2.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 0x12;
+		int c = 012;
+		int d = 0x1a;
+		int e = 0b011;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
