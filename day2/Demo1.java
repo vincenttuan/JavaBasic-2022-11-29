2ed8829005590a50c48906fd1e67925d3c36b7cf
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-01 19:20:08 +0800

    Add files via upload

---
+++ day2/Demo1.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		byte a = 10;
+		short b = 28000;
+		int c = 100_0000;
+		long d = 200_0000_0000L;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
