db95eb730bafc85347be861b4cb355440c8f0449
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-01 20:33:46 +0800

    Add files via upload

---
+++ day2/Demo4.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		
+		char c1 = 'A';
+		char c2 = 65;
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c1 + c2);
+
+		String s1 = "A";
+		String s2 = "A";
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1 + s2);
+
+	}
+
+}
