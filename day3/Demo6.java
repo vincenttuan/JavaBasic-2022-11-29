ab169199d972faa69e7ec3f682fff86dd0b39648
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-06 19:31:55 +0800

    Add files via upload

---
+++ day3/Demo6.java
@@ -0,0 +1,14 @@
+package day3;
+
+import java.util.Random;
+
+public class Demo6 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int n = random.nextInt(100); // 0~99 之間取一個整數亂數(隨機數)
+		System.out.println(n);
+
+	}
+
+}
