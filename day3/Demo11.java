06338e9f8ea4af597ab76def1fb59409049c184a
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-06 21:26:39 +0800

    Add files via upload

---
+++ day3/Demo11.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class Demo11 {
+
+	public static void main(String[] args) {
+		// ~ 補數公式: -(x)-1
+		int x = 13;
+		int y = ~x;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
