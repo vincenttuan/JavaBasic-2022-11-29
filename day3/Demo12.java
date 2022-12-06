5699600e21627dd4eb040aeea87053e6f6b361e6
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-06 21:41:21 +0800

    Add files via upload

---
+++ day3/Demo12.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class Demo12 {
+	public static void main(String[] args) {
+		// 位移運算子 <<, >>, >>>
+		System.out.println(5 << 1);
+		System.out.println(5 >> 1);
+		System.out.println(5 >>> 1);
+
+		System.out.println(-5 >> 1);
+		System.out.println(-5 >>> 1);
+	}
+}
