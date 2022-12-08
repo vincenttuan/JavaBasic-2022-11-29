6310a57054955103ad0c339ddf5828c7feae846e
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-08 19:31:44 +0800

    Add files via upload

---
+++ day4/Demo5.java
@@ -0,0 +1,18 @@
+package day4;
+
+import java.util.Scanner;
+
+public class Demo5 {
+	/*
+	 * 使用者輸入身高與體重
+	 * 系統可以印出 "bmi 值" 與 "bmi 值的說明"
+	 * bmi 值的說明:
+	 * bmi < 18       -> 印出 "過輕"
+	 * 18 <= bmi < 23 -> 印出 "正常"
+	 * bmi >= 23      -> 印出 "過重"
+	*/
+	public static void main(String[] args) {
+		// 請撰寫 ...
+
+	}
+}
