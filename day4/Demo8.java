2c2ad2f4023e9e9cbb4faffaffe8305adcd148a0
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-08 21:24:20 +0800

    Add files via upload

---
+++ day4/Demo8.java
@@ -0,0 +1,48 @@
+package day4;
+
+import java.util.Scanner;
+
+public class Demo8{
+	/*  score    等級
+	 * 90 ~ 100   A
+	 * 80 ~ 89    B
+	 * 70 ~ 79    C
+	 * 60 ~ 69    D
+	 * 0  ~ 59    E
+	 * other -> "score error"
+	*/
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("請輸入分數: ");
+		int score = sc.nextInt();
+		// 判斷資料範圍是否是在 0~100 之間 ?
+		if(!(score >= 0 && score <= 100)) {
+			System.out.println("成績錯誤");
+			return; // 結束
+		}
+
+
+		// 請利用 switch-case 來印出 score 的(A~E)等級
+		int level = score / 10;
+		switch(level) {
+			case 10:
+			case 9:
+				System.out.println("A");
+				break;
+			case 8:
+				System.out.println("B");
+				break;
+			case 7:
+				System.out.println("C");
+				break;
+			case 6:
+				System.out.println("D");
+				break;
+			default:
+				System.out.println("E");
+				//break;
+		}
+
+	}
+
+}
