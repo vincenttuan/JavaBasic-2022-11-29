d3212d545f2d51992bdef73e5aac50740964fa21
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-08 20:55:01 +0800

    Add files via upload

---
+++ day4/Demo7.java
@@ -0,0 +1,22 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Demo7{
+	/*  score    等級
+	 * 90 ~ 100   A
+	 * 80 ~ 89    B
+	 * 70 ~ 79    C
+	 * 60 ~ 69    D
+	 * 0  ~ 59    E
+	 * other -> "score error"
+	*/
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("請輸入分數: ");
+		int score = sc.nextInt();
+		// 請利用 switch-case 來印出 score 的(A~E)等級
+
+	}
+
+}
