db043477b8a0f941caf83983dbd8da0563041522
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-08 21:13:36 +0800

    Add files via upload

--- day4/Demo7.java
+++ day4/Demo7.java
@@ -15,7 +15,34 @@ public class Demo7{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("請輸入分數: ");
 		int score = sc.nextInt();
-		// 請利用 switch-case 來印出 score 的(A~E)等級
+		// 判斷資料範圍是否是在 0~100 之間 ?
+		if(score >= 0 && score <= 100) {
+
+			// 請利用 switch-case 來印出 score 的(A~E)等級
+			int level = score / 10;
+			switch(level) {
+				case 10:
+				case 9:
+					System.out.println("A");
+					break;
+				case 8:
+					System.out.println("B");
+					break;
+				case 7:
+					System.out.println("C");
+					break;
+				case 6:
+					System.out.println("D");
+					break;
+				default:
+					System.out.println("E");
+			}
+
+		} else {
+			System.out.println("分數錯誤");
+		}
+
+
 
 	}
 
