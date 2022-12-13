f69815da5c8abdc94b529de81254c1d03f5ccb86
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-13 21:03:36 +0800

    Add files via upload

--- day5/GuessNumber.java
+++ day5/GuessNumber.java
@@ -1,11 +1,13 @@
 package day5;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessNumber {
 
 	public static void main(String[] args) {
-		int ans = 77;
+		Random random = new Random();
+		int ans = random.nextInt(99) + 1; // 1~99
 		int min = 0;
 		int max = 100;
 
@@ -14,7 +16,12 @@ public class GuessNumber {
 			Scanner scanner = new Scanner(System.in);
 			System.out.printf("請 user 在 %d ~ %d 之間猜一數字: ", min, max);
 			int userGuess = scanner.nextInt();
-			// 判斷是否有猜中 ?
+			// 判斷所猜的數字是否在提示範圍內 ?
+			if(userGuess <= min || userGuess >= max) {
+				System.out.println("範圍錯誤請重猜~");
+				continue;
+			}
+			// 判斷 user 是否有猜中 ?
 			if(userGuess < ans) {
 				min = userGuess;
 			} else if(userGuess > ans) {
@@ -23,7 +30,18 @@ public class GuessNumber {
 				System.out.println("Bingo 恭喜 user 答對了");
 				break;
 			}
-
+			// pc 猜
+			int pcGuess = random.nextInt(max-min-1) + min + 1;
+			System.out.printf("請 pc 在 %d ~ %d 之間猜一數字: %d\n", min, max, pcGuess);
+			// 判斷 pc 是否有猜中 ?
+			if(pcGuess < ans) {
+				min = pcGuess;
+			} else if(pcGuess > ans) {
+				max = pcGuess;
+			} else {
+				System.out.println("Bingo 恭喜 pc 答對了");
+				break;
+			}
 
 		} while(true);
 
