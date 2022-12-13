998f420e568b9ed3a2fd1deffaee809f6dc30a5d
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-13 20:28:42 +0800

    Add files via upload

---
+++ day5/DoWhileDemo.java
@@ -0,0 +1,14 @@
+package day5;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		int count = 0;
+		do {
+			System.out.println("Java");
+			count--;
+		} while(count > 0);
+
+	}
+
+}
