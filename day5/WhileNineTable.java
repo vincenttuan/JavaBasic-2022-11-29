2d6c6d917dc0a1533f83adc70c289ec5c79700a7
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-13 19:34:04 +0800

    Add files via upload

--- day5/WhileNineTable.java
+++ day5/WhileNineTable.java
@@ -3,13 +3,15 @@ package day5;
 public class WhileNineTable {
 
 	public static void main(String[] args) {
-		
+		int n = 13;
+
 		int x = 1;
-		while(x <= 9) {
+		while(x <= n) {
 
 			int y = 1;
-			while(y <= 9) {
-				System.out.printf("%d x %d = %d  ", x, y, (x*y));	
+			while(y <= n) {
+				//System.out.printf("%2d x %2d = %-3d  ", x, y, (x*y));	
+				System.out.printf("%2d x %-2d = %3d  ", x, y, (x*y));	
 				y++;
 			}
 			System.out.println();
