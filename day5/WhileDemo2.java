e4997b5ccd72ba2723b1472760ed5a5dc528e576
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-13 19:01:01 +0800

    Add files via upload

---
+++ day5/WhileDemo2.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class WhileDemo2 {
+
+	public static void main(String[] args) {
+		
+		int count = 5;
+
+		while(count-- > 0) {
+			System.out.println("Java");
+			//count--;
+		}
+
+	}
+
+}
