1efe3bf143038e929bc6537476cd43e3d6d20fea
VincentTuan <vincentjava@yahoo.com.tw>
2022-12-13 19:05:13 +0800

    Add files via upload

---
+++ day5/WhileDemo3.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class WhileDemo3 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 10;
+		while(x < y) {
+			System.out.printf("x=%d, y=%d\n", x, y);
+			System.out.println("Java");
+			x++;
+			y--;
+		}
+
+	}
+
+}
